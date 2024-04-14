e13db3d251c25e861f4fda02e9cef2a04cc0dca5
Jonas Bjarnehall <ssepheron@MSI>
2024-04-14 19:03:10 +0200

    kmom02 klar

--- src/Controller/CardGame.php
+++ src/Controller/CardGame.php
@@ -172,6 +172,37 @@ class CardGame extends AbstractController
         ]);
     }
     /**
+     * Displays a single card as a JSON response.
+     *
+     * @return JsonResponse
+     */
+    #[Route("/api/deck/draw", name: "api_deck_draw")]
+    public function apiDrawCard(SessionInterface $session): Response
+    {
+        if (!$session->has('shuffledDeck')) {
+            $deck = new DeckOfCards();
+            $deck->shuffle();
+            $session->set('shuffledDeck', serialize($deck->getShuffledCards()));
+        }
+
+        $deck = unserialize($session->get('shuffledDeck'));
+        if (empty($deck)) {
+            return $this->redirectToRoute('api_deck_shuffle');
+        }
+
+        $card = array_shift($deck);
+        $session->set('shuffledDeck', serialize($deck));
+
+        $cardData = [
+            'suit' => $card->getSuit(),
+            'value' => $card->getValue(),
+            'remaining' => count($deck)
+        ];
+
+        return new JsonResponse($cardData);
+    }
+
+    /**
      * Draws multiple cards from the deck.
      *
      * @param SessionInterface
@@ -197,6 +228,40 @@ class CardGame extends AbstractController
 
         return $this->render('card/draw_cards.html.twig', [
             'card' => $cardsDrawn,
+            'remaining' => count($deck)
+        ]);
+    }
+    /**
+     * Displays a multiple cards as a JSON response.
+     *
+     * @return JsonResponse
+     */
+    #[Route("api/deck/draw/{number}", name: "api_card_draw_cards")]
+    public function apiDrawCards(SessionInterface $session, int $number): Response
+    {
+        if (!$session->has('shuffledDeck')) {
+            $deck = new DeckOfCards();
+            $deck->shuffle();
+            $session->set('shuffledDeck', serialize($deck->getShuffledCards()));
+        }
+
+        $deck = unserialize($session->get('shuffledDeck'));
+        if (count($deck) < $number) {
+            return $this->redirectToRoute('api_deck_shuffle');
+        }
+
+        $cardsDrawn = array_splice($deck, 0, $number);
+        $session->set('shuffledDeck', serialize($deck));
+
+        $cardsData = array_map(function ($card) {
+            return [
+                'suit' => $card->getSuit(),
+                'value' => $card->getValue()
+            ];
+        }, $cardsDrawn);
+
+        return new JsonResponse([
+            'cards' => $cardsData,
             'remaining' => count($deck)
         ]);
     }
