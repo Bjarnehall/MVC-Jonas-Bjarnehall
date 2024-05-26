7353136d40a5655877aa01b44e6a16db8c47fd22
Jonas Bjarnehall <ssepheron@MSI>
2024-05-26 09:12:56 +0200

    continued testing

--- tests/Adventure/AdventureControllerTest.php
+++ tests/Adventure/AdventureControllerTest.php
@@ -71,4 +71,45 @@ class AdventureControllerTest extends WebTestCase
         }
     }
 
+    public function testAdventureAddCd(): void
+    {
+        $adventureInventoryMock = $this->createMock(AdventureInventory::class);
+        $adventures = [new Adventure(), new Adventure()];
+        $adventureInventoryMock->method('getAllAdventures')->willReturn($adventures);
+
+        $client = static::createClient();
+        $container = $client->getContainer();
+
+        $container->set(AdventureInventory::class, $adventureInventoryMock);
+        $client->request('GET', '/adventure/addcd');
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+
+        foreach ($adventures as $adventure) {
+            $notes = $adventure->getNotes();
+            if ($notes !== null) {
+                $this->assertStringContainsString($notes, $client->getResponse()->getContent());
+            }
+        }
+    }
+
+    public function testAdventureAdd(): void
+    {
+        $adventureInventoryMock = $this->createMock(AdventureInventory::class);
+        $adventures = [new Adventure(), new Adventure()];
+        $adventureInventoryMock->method('getAllAdventures')->willReturn($adventures);
+
+        $client = static::createClient();
+        $container = $client->getContainer();
+
+        $container->set(AdventureInventory::class, $adventureInventoryMock);
+        $client->request('GET', '/adventure/add');
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+
+        foreach ($adventures as $adventure) {
+            $notes = $adventure->getNotes();
+            if ($notes !== null) {
+                $this->assertStringContainsString($notes, $client->getResponse()->getContent());
+            }
+        }
+    }
 }
