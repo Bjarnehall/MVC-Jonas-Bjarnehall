9ed6202ac126a0a2ec71f993e36b73687b5e167a
Jonas Bjarnehall <ssepheron@MSI>
2024-05-29 06:05:05 +0200

    testing Adventure Grades

--- tests/Adventure/AdventureGradesTest.php
+++ tests/Adventure/AdventureGradesTest.php
@@ -3,6 +3,7 @@
 use App\Adventure\AdventureGrades;
 use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Framework\TestCase;
+use App\Entity\Grades;
 
 class AdventureGradesTest extends TestCase
 {
@@ -30,4 +31,46 @@ class AdventureGradesTest extends TestCase
 
         $this->assertEquals($expectedPersons, $this->adventureGrades->getPersons());
     }
+
+    public function testAddGrades(): void
+    {
+        $entityManager = $this->createMock(EntityManagerInterface::class);
+
+        $entityManager->expects($this->exactly(3))
+            ->method('persist')
+            ->with($this->isInstanceOf(Grades::class));
+
+        $entityManager->expects($this->once())
+            ->method('flush');
+
+        $adventureGrades = new AdventureGrades($entityManager);
+
+        $persons = [
+            ['name' => 'Johan Andersson', 'course' => 'DATABASE', 'grade' => 'MVG'],
+            ['name' => 'Anita Karlsson', 'course' => 'DATABASE', 'grade' => 'VG'],
+            ['name' => 'Sture Snesteg', 'course' => 'DATABASE', 'grade' => 'IG'],
+        ];
+
+        $adventureGrades->addGrades($persons);
+    }
+    // public function addGrades(array $persons): void
+    // {
+    //     foreach ($persons as $person) {
+    //         $name = $person['name'];
+    //         $course = $person['course'];
+    //         $grade = $person['grade'];
+
+    //         if (!$name || !$course || !$grade) {
+    //             continue;
+    //         }
+
+    //         $grades = new Grades();
+    //         $grades->setName($name)
+    //                ->setCourse($course)
+    //                ->setGrade($grade);
+    //         $this->entityManager->persist($grades);
+    //     }
+
+    //     $this->entityManager->flush();
+    // }
 }
